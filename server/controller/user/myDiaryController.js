4e9f87ab6636e9a2456b768faf8f46f03c2e032a
shine <arinze@winexviv.com>
2018-07-19 22:59:01 +0000

    API endpoint to get all entries

--- server/controller/user/myDiaryController.js
+++ server/controller/user/myDiaryController.js
@@ -1,3 +1,5 @@
+import winston from 'winston';
+
 const myDiary = [
   {
     id: 1,
@@ -6,7 +8,8 @@ const myDiary = [
   },
   {
     id: 2,
-    Title: 'Falling in love',
+    // Title: '                  Falling in love',
+    Title: '      ',
     entry: 'Everything happened with millsecond, beautiful girl passsing by with alovely smile',
   },
 ];
@@ -21,6 +24,35 @@ class myDiaryController {
       res.status(201).json({
         success: 'entry created successfully',
         result: myDiary,
+      });
+    }
+  }
+
+  static getAllEntries(req, res) {
+    // const entry = myDiary.map(s => s.trim());
+    const entry = myDiary;
+    const result = [];
+    if (!entry) {
+      res.status(404).json({ message: 'No Entry Available' });
+    } else {
+      winston.log('info', entry.length);
+      for (let i = 0; i < entry.length; i += 1) {
+        const check = (entry[i].Title).trim();
+        if (!check) {
+          let pickEntry = entry[i].entry;
+          let pickEntryArray = pickEntry.split(' ');
+          winston.log('info', pickEntryArray);
+          if(pickEntryArray.length > 10) {
+            pickEntryArray = pickEntryArray.slice(0, 10);
+            pickEntry = pickEntryArray.join(' ')+'...';
+            entry[i].Title = pickEntry;
+          }
+        }
+        result.push(entry[i]);
+      }
+      res.status(201).json({
+        success: 'success',
+        result,
       });
     }
   }
---
+++ server/test/getAllEntry.js
@@ -0,0 +1,31 @@
+import chaiHttp from 'chai-http';
+import chai, { expect } from 'chai';
+import { describe } from 'mocha';
+import app from '../../app';
+
+chai.use(chaiHttp);
+chai.should();
+describe('TESTING GET ALL ENTRY ENDPOINT', () => {
+  describe('get /entries', () => {
+    it('should list ALL entry on /entries GET ', (done) => {
+      chai.request(app)
+        .get('/api/v1/entries')
+        .end((error, response) => {
+            response.should.have.status(201);
+            response.body.result.should.be.a('array');
+            response.body.should.have.property('success').eql('success');
+          done();
+        });
+    });
+    // it('should return an error message', (done) => {
+    //   chai.request(app)
+    //     .get('/api/v1/entries')
+    //     .end((error, response) => {
+    //       response.should.have.status(404);
+    //       response.body.should.have.property('message');
+    //       response.body.message.should.eql('No Entry Available');
+    //       done();
+    //     });
+    // });
+  });
+});
