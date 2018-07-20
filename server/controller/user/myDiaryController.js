00182f8ac7d44b9761b7bce574b808565090c9bb
shine <arinze@winexviv.com>
2018-07-20 02:03:39 +0000

    getAllEntry function refactored

--- server/controller/user/myDiaryController.js
+++ server/controller/user/myDiaryController.js
@@ -29,32 +29,36 @@ class myDiaryController {
   }
 
   static getAllEntries(req, res) {
-    // const entry = myDiary.map(s => s.trim());
     const entry = myDiary;
-    const result = [];
     if (!entry) {
       res.status(404).json({ message: 'No Entry Available' });
     } else {
-      winston.log('info', entry.length);
-      for (let i = 0; i < entry.length; i += 1) {
-        const check = (entry[i].Title).trim();
-        if (!check) {
-          let pickEntry = entry[i].entry;
-          let pickEntryArray = pickEntry.split(' ');
-          winston.log('info', pickEntryArray);
-          if(pickEntryArray.length > 10) {
-            pickEntryArray = pickEntryArray.slice(0, 10);
-            pickEntry = pickEntryArray.join(' ')+'...';
-            entry[i].Title = pickEntry;
-          }
-        }
-        result.push(entry[i]);
-      }
+      const result = myDiaryController.processEntry(entry);
       res.status(201).json({
         success: 'success',
         result,
       });
     }
+  }
+
+  static processEntry(entries) {
+    const entry = entries;
+    const result = [];
+    winston.log('info', entry.length);
+    for (let i = 0; i < entry.length; i += 1) {
+      const check = (entry[i].Title).trim();
+      if (!check) {
+        let pickEntry = entry[i].entry;
+        let pickEntryArray = pickEntry.split(' ');
+        if (pickEntryArray.length > 10) {
+          pickEntryArray = pickEntryArray.slice(0, 10);
+          pickEntry = `${pickEntryArray.join(' ')}...`;
+          entry[i].Title = pickEntry;
+        }
+      }
+      result.push(entry[i]);
+    }
+    return result;
   }
 }
 export default myDiaryController;
