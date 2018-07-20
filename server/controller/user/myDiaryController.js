8d8ee5c0ed31a21a2a08786c14456a9e195bf20f
shine <arinze@winexviv.com>
2018-07-20 22:35:16 +0000

    Endpoint to get all entry created

--- server/controller/user/myDiaryController.js
+++ server/controller/user/myDiaryController.js
@@ -25,5 +25,37 @@ class myDiaryController {
       });
     }
   }
+
+  static getAllEntries(req, res) {
+    const entry = myDiary;
+    if (!entry) {
+      res.status(404).json({ message: 'No Entry Available' });
+    } else {
+      const result = myDiaryController.processData(entry);
+      res.status(201).json({
+        success: 'success',
+        result,
+      });
+    }
+  }
+
+  static processData(data) {
+    const entry = data;
+    const result = [];
+    for (let i = 0; i < entry.length; i += 1) {
+      const check = (entry[i].Title).trim();
+      if (!check) {
+        let pickEntry = entry[i].entry;
+        let pickEntryArray = pickEntry.split(' ');
+        if (pickEntryArray.length > 10) {
+          pickEntryArray = pickEntryArray.slice(0, 10);
+          pickEntry = `${pickEntryArray.join(' ')}...`;
+          entry[i].Title = pickEntry;
+        }
+      }
+      result.push(entry[i]);
+    }
+    return result;
+  }
 }
 export default myDiaryController;
