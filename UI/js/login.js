b1c3c77f83f405c9631c200a53b7aab51474f3b6
shine <arinze@winexviv.com>
2018-08-01 21:17:50 +0000

    refactoring of login functions

--- UI/js/login.js
+++ UI/js/login.js
@@ -2,40 +2,34 @@ const emailField = document.getElementById('email');
 const passwordField = document.getElementById('password');
 const url = '/api/v1/auth/login';
 
-const validateInput = (data) => {
+const validateErrorMessage = (data) => {
   if (data.message) {
     const loginMessage = document.getElementById('loginMessage');
     loginMessage.style.display = 'block';
     loginMessage.innerHTML = data.message;
-  } else {
-    const emailError = document.getElementById('emailError');
-    const passwordError = document.getElementById('passwordError');
-    const { email, password } = data.errors;
-    if (email) {
-      emailError.style.display = 'block';
-      emailError.innerHTML = email;
-    }
-    if (password) {
-      passwordError.style.display = 'block';
-      passwordError.innerHTML = password;
-    }
   }
 };
-const getfetch = (content) => {
-  fetch(url, content)
-    .then(data => data.json())
-    .then((response) => {
-      if (response.success === false) {
-        validateInput(response);
-      } else {
-        localStorage.setItem('authToken', `Bearer ${response.token}`);
-        const roller = document.getElementById('loginSuccess');
-        roller.innerHTML = 'Login was successful...';
-        setTimeout(() => {
-          window.location.href = 'http://127.0.0.1:4500/allEntry.html';
-        }, 3000);
-      }
-    });
+const validateErrorField = (data) => {
+  const emailError = document.getElementById('emailError');
+  const passwordError = document.getElementById('passwordError');
+  const { email, password } = data.errors;
+  if (email) {
+    emailError.style.display = 'block';
+    emailError.innerHTML = email;
+  }
+  if (password) {
+    passwordError.style.display = 'block';
+    passwordError.innerHTML = password;
+  }
+};
+
+const successLogin = (response) => {
+  localStorage.setItem('authToken', `Bearer ${response.token}`);
+  const roller = document.getElementById('loginSuccess');
+  roller.innerHTML = 'Login was successful...';
+  setTimeout(() => {
+    window.location.href = 'http://127.0.0.1:4500/allEntry.html';
+  }, 3000);
 };
 
 const login = () => {
@@ -50,5 +44,14 @@ const login = () => {
       'Content-Type': 'application/json',
     },
   };
-  getfetch(content);
+  fetch(url, content)
+    .then(data => data.json())
+    .then((response) => {
+      if (response.success === false) {
+        validateErrorMessage(response);
+        validateErrorField(response);
+      } else {
+        successLogin(response);
+      }
+    });
 };
