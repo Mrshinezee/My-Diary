6067cc8b3322b49f53b74345b2fa2b2165202ec9
shine <arinze@winexviv.com>
2018-08-17 12:36:38 +0000

    Project documentation added

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import winston from 'winston';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+
 
 import router from './server/routes';
 
 const app = express();
 const port = process.env.PORT || 4500;
+const swaggerDocument = YAML.load(`${process.cwd()}/swagger.yaml`);
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('UI'));
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 router(app);
 app.listen(port);
