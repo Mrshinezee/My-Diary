7ee3d404bb2217311d37ab6cccf7e0b36dfa5a0d
Mrshinezee <ijeomaharinze@gmail.com>
2019-07-31 18:36:27 +0100

    fetch first 10 coins

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,6 @@
 import AuthUsersController from '../controllers/authUsersController';
 import MyDiaryController from '../controllers/myDiaryController';
+import CoinController from '../controllers/coinController';
 import UserMiddleware from '../middleware/user';
 import userValidation from '../middleware/validate';
 
@@ -8,6 +9,7 @@ const userRoute = (app) => {
   app.get('/api/v1/user', UserMiddleware.validToken, userValidation.getUserDetail);
   // app.get('/api/v1/entries/:offSet', UserMiddleware.validToken, MyDiaryController.getAllEntries);
   app.get('/api/v1/entries', UserMiddleware.validToken, MyDiaryController.getAllEntries);
+  app.get('/api/v1/latest', UserMiddleware.validToken, CoinController.getLatestCurrency);
   app.get('/api/v1/entry/:entryId', UserMiddleware.validToken, userValidation.validateParam, MyDiaryController.getEntryById);
   app.put('/api/v1/entries/:entryId', UserMiddleware.validToken, userValidation.validateParam, userValidation.validateEntry, MyDiaryController.editEntry);
   app.delete('/api/v1/entries/:entryId', UserMiddleware.validToken, userValidation.validateParam, MyDiaryController.deleteEntry);
